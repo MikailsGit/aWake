ad59eab5c1b91e60509bfa80b5363adc4651363b
Mikail YAVUZ <mikailyavuz751900@gmail.com>
2022-04-29 16:03:50 +0200

    Initial Commit

---
+++ aWake!/aWake_App.swift
@@ -0,0 +1,17 @@
+//
+//  aWake_App.swift
+//  aWake!
+//
+//  Created by Mikail Yavuz on 29/04/22.
+//
+
+import SwiftUI
+
+@main
+struct aWake_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
