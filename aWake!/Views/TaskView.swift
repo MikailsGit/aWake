568a68fa1620d5dfd2819006024513a2588ecdc0
Mikail YAVUZ <mikailyavuz751900@gmail.com>
2022-05-06 16:12:13 +0200

    test

--- aWake!/Views/TaskView.swift
+++ aWake!/Views/TaskView.swift
@@ -86,7 +86,6 @@ struct TaskView: View {
                             
                         default:
                             break
-                        
                         }
                     }
                 }
